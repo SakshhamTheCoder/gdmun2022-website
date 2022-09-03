712f711861dda2912eeea40a80f4b3b79bfe6230
Sakshham Bhagat <sakshhamtg@gmail.com>
2022-09-03 20:48:37 +0530

    Updated website .1

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ var x = setInterval(function () {
 
 
 // var i = 0;
-// var txt1 = 'We are glad to announce the 2nd edition of GDMUN. This edition hopes to improve upon the pre-existing MUN circuit, bringing emphasis upon the participation of budding leaders in Model United Nations. We also share our campus\' aim of becoming Carbon Neutral in the future by being the first Green MUN in the circuit. The last edition was a huge success, with over 300 delegates participating from the top schools in Jammu. This year, we want to top that number, with our 8 committees giving the beginners a chance to explore the different aspects of MUN.<br><br>The secretariats wishes you all the very best and hopes that our conference brings you joy.';
+// var txt1 = 'We are glad to announce the 2nd edition of GDMUN. This edition hopes to improve upon the pre-existing MUN circuit, bringing emphasis upon the participation of budding leaders in Model United Nations. We also share our campus\' aim of becoming Carbon Neutral in the future by being the first Green MUN in the circuit. The last edition was a huge success, with over 300 delegates participating from the top schools in Jammu. This year, we want to top that number, with our 8 committees giving the beginners a chance to explore the different aspects of MUN.<br><br>The secretariat wishes you all the very best and hopes that our conference brings you joy.';
 // var speed = 20; /* The speed/duration of the effect in milliseconds */
 
 // function typeWriter() {
